5a3b27e5f108a38957a98898538cff687bc4a2a6
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-10-07 01:50:50 +0900

    TCID-회원가입_예외_02

--- signup_test.py
+++ signup_test.py
@@ -59,7 +59,7 @@ def test_signup_success(driver):
 def test_signup_duplicate_email(driver):
     driver.get('https://www.coupang.com/')
 
-    try:
+    try: 
         duplicate_signup_click = driver.find_element(By.LINK_TEXT, '회원가입')
         duplicate_signup_click.click()
         print('회원가입 페이지 이동')
@@ -78,24 +78,33 @@ def test_signup_duplicate_email(driver):
         print(f'오류발생 : {e}')
 
 
-# 유요하지 않은 이메일
+# 유효하지 않은 이메일
 def test_signup_invalid_email(driver):
     driver.get('https://www.coupang.com/')
 
     try:
-        invalid_signup_click = driver.find_element(By.LINK_TEXT, '회원가입')
-        invalid_signup_click.click()
+        signup_click = WebDriverWait(driver, 10). until(
+            EC.element_to_be_clickable((By.LINK_TEXT, '회원가입'))
+        )
+        signup_click.click()
         print('회원가입 페이지 이동')
 
         invalid_email = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID,'join-email-input'))
         )
         invalid_email.send_keys('pjcu011@g@ail.com')
-        print('유요하지 않은 이메일 작성')
+        print('유효하지 않은 이메일 작성')
         time.sleep(2)
 
         invalid_email.send_keys(Keys.TAB)
         print('TAB으로 비밀번호 칸 이동')
+
+        error_massage = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.member__message-area.member__message-area--error'))
+        )
+
+        assert '이메일을 올바르게 입력해주세요.' in error_massage.text
+        print('테스트 성공 : 정확한 에러 메시지가 노출 되었습니다.')
 
     except Exception as e:
         print(f'오류발생 : {e}')
@@ -128,6 +137,6 @@ def test_signup_invalid_password(driver):
 if __name__ == '__main__':
     # test_signup_success(driver)
     # test_signup_duplicate_email(driver)
-    # test_signup_invalid_email(driver)
-    test_signup_invalid_password(driver)
+    test_signup_invalid_email(driver)
+    # test_signup_invalid_password(driver)
     input()
